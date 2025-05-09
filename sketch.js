2fd63b5ede23145a24efbd07c95ff764a2d74258
liuchenying0429 <413730077@o365.tku.edu.tw>
2025-05-09 10:54:50 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -67,16 +67,13 @@ function draw() {
         drawLines(hand.keypoints, 13, 16);
         drawLines(hand.keypoints, 17, 20);
 
-        // 檢測食指（keypoint 8）與大拇指（keypoint 4）是否同時碰觸圓的邊緣
+        // 檢測食指（keypoint 8）是否碰觸圓
         let indexFinger = hand.keypoints[8];
-        let thumb = hand.keypoints[4];
-        let dIndex = dist(indexFinger.x, indexFinger.y, circleX, circleY);
-        let dThumb = dist(thumb.x, thumb.y, circleX, circleY);
-
-        if (dIndex < circleRadius && dThumb < circleRadius) {
-          // 如果兩者同時碰觸，讓圓跟隨手指移動
-          circleX = (indexFinger.x + thumb.x) / 2; // 圓心移動到兩點的中間
-          circleY = (indexFinger.y + thumb.y) / 2;
+        let d = dist(indexFinger.x, indexFinger.y, circleX, circleY);
+        if (d < circleRadius) {
+          // 如果碰觸，讓圓跟隨食指移動
+          circleX = indexFinger.x;
+          circleY = indexFinger.y;
         }
       }
     }
