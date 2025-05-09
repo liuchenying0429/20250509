ab59f27cbce5607180e54aa4d1672b2f468af292
liuchenying0429 <413730077@o365.tku.edu.tw>
2025-05-09 10:29:11 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function gotHands(results) {
 }
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(640, 480); //產生一個畫布，640*480
   video = createCapture(VIDEO, { flipped: true });
   video.hide();
 
@@ -48,7 +48,25 @@ function draw() {
           noStroke();
           circle(keypoint.x, keypoint.y, 16);
         }
+
+        // Draw lines connecting keypoints in groups
+        drawLines(hand.keypoints, 0, 4);  // Connect keypoints 0 to 4
+        drawLines(hand.keypoints, 5, 8);  // Connect keypoints 5 to 8
+        drawLines(hand.keypoints, 9, 12); // Connect keypoints 9 to 12
+        drawLines(hand.keypoints, 13, 16); // Connect keypoints 13 to 16
+        drawLines(hand.keypoints, 17, 20); // Connect keypoints 17 to 20
       }
     }
+  }
+}
+
+// Helper function to draw lines between keypoints
+function drawLines(keypoints, startIdx, endIdx) {
+  stroke(0, 255, 0); // Set line color
+  strokeWeight(2);   // Set line thickness
+  for (let i = startIdx; i < endIdx; i++) {
+    let kp1 = keypoints[i];
+    let kp2 = keypoints[i + 1];
+    line(kp1.x, kp1.y, kp2.x, kp2.y);
   }
 }
